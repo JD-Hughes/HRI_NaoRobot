dc90c3c97a80959de68f145022b60f010b0a0868
AhmedDeib <ahmedeldeib2001@gmail.com>
2024-12-12 20:21:28 +0000

    choregraphe
    updating the connection in choregraphe

--- Final Demo/Choregraphe/translations/translation_en_US.ts
+++ Final Demo/Choregraphe/translations/translation_en_US.ts
@@ -208,10 +208,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
-            <translation>Hello</translation>
+            <translation type="vanished">Hello</translation>
         </message>
     </context>
     <context>
@@ -243,6 +242,33 @@
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Their Turn</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Their Turn (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Their Turn (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Their Turn (3)</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
