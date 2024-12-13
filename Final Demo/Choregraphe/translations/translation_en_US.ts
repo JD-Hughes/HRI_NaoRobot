579f225ab685e6c45737fa1572473024382cd288
AhmedDeib <ahmedeldeib2001@gmail.com>
2024-12-13 11:57:19 +0000

    Updated choreographe
    Co-Authored-By: Jamie Hughes <1339246+JD-Hughes@users.noreply.github.com>

--- Final Demo/Choregraphe/translations/translation_en_US.ts
+++ Final Demo/Choregraphe/translations/translation_en_US.ts
@@ -270,10 +270,9 @@
     <context>
         <name>behavior_1/behavior.xar:/Their Turn (3)</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hello</translation>
+            <translation type="obsolete">Hello</translation>
         </message>
     </context>
     <context>
